b88553b1e3395bdc07b26b487e3f5dfc1c968e32
Adrian Barradas <adrian.barradas@gmail.com>
2020-04-13 18:57:57 -0500

    Update

--- connection.R
+++ connection.R
@@ -2,9 +2,29 @@
 library(RMariaDB)
 
 rmariadb.settingsfile<-"claves.R"
-storiesDb<-dbConnect(RMariaDB::MariaDB(),user=userDB, password=passwordDB, host=hostDB)
+storiesDb<-dbConnect(RMariaDB::MariaDB(),user=userDB, password=passwordDB, host=hostDB, db="Bisi_raw")
 dbListTables(storiesDb)
 
+#Queies transfromación y carga de datos en Modelo multidimensional
+#Transformación tabla viajes @Bisi_raw se inserta en tabla viaje @Bisi
+query<-paste(
+  "INSERT INTO Bisi.viaje
+  SELECT 
+  SUBSTR(Inicio_del_viaje, 1 , 10 ) Fecha, 
+  Viaje_Id, 
+  Usuario_Id, 
+  Genero, 
+  Inicio_del_viaje,
+  Fin_del_viaje, 
+  Origen_Id,
+  Destino_Id,
+  SUBTIME( CONVERT (SUBSTR(Fin_del_viaje, 11 , 9 ),TIME),
+  ( CONVERT (SUBSTR(Inicio_del_viaje, 11 , 9 ),TIME))) Duracion_viaje
+  FROM Bisi_raw.viajes;")
+
+
+dbRows<-dbExecute(storiesDb,query)
+dbRows
 
 # Desconexión de la base de datos
 dbDisconnect(storiesDb)
